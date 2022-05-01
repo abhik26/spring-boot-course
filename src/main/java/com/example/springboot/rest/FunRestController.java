a9ab47d77f8bf2943e5955fd72aaefdf7bfacdf8
Abhishek Anand <46523310+abhik26@users.noreply.github.com>
2022-05-01 22:39:42 +0530

    initial commit

---
+++ src/main/java/com/example/springboot/rest/FunRestController.java
@@ -0,0 +1,39 @@
+package com.example.springboot.rest;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+	
+	private final String teamName;
+	private final String coachName;
+	
+	FunRestController(@Value("${team.name}") String teamName, @Value("${coach.name}") String coachName) {
+		this.teamName = teamName;
+		this.coachName = coachName;
+	}
+
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello World! Time on server is: " + LocalDateTime.now();
+	}
+	
+	@GetMapping("/workout")
+	public String getDailyWorkout() {
+		return "Run a hard 5k!";
+	}
+	
+	@GetMapping("/fortune")
+	public String getFortune() {
+		return "Today is your lucky day!";
+	}
+	
+	@GetMapping("/teaminfo")
+	public String getTeamInfo() {
+		return "Coach name: " + coachName + ", team name: " + teamName;
+	}
+}
