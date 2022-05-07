4d9e6b6eb16db9c756a7ca647c406ce1ab62996d
Abhishek Anand <46523310+abhik26@users.noreply.github.com>
2022-05-07 20:36:06 +0530

    Add spring boot starter rest

--- src/main/java/com/example/springboot/dao/EmployeeRepository.java
+++ src/main/java/com/example/springboot/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.example.springboot.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.example.springboot.entity.Employee;
 
+@RepositoryRestResource(path="assets")
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
 }
--- src/main/java/com/example/springboot/rest/EmployeeRestController.java
+++ src/main/java/com/example/springboot/rest/EmployeeRestController.java
@@ -1,57 +1,57 @@
-package com.example.springboot.rest;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.example.springboot.entity.Employee;
-import com.example.springboot.service.EmployeeService;
-
-@RestController
-@RequestMapping("/api")
-public class EmployeeRestController {
-
-	private final EmployeeService employeeService;
-	
-	@Autowired
-	public EmployeeRestController(EmployeeService employeeDAO) {
-		super();
-		this.employeeService = employeeDAO;
-	}
-	
-	@GetMapping("/employees")
-	public List<Employee> getEmployees() {
-		return employeeService.findAll();
-	}
-	
-	@GetMapping("/employees/{employeeId}")
-	public Employee getEmployee(@PathVariable("employeeId") Long employeeId) {
-		return employeeService.findById(employeeId);
-	}
-	
-	@PostMapping("/employees")
-	public Employee saveEmployee(@RequestBody Employee employee) {
-		employeeService.saveOrUpdate(employee);
-		return employee;
-	}
-	
-	@PutMapping("/employees")
-	public Employee updateEmployee(@RequestBody Employee employee) {
-		employeeService.saveOrUpdate(employee);
-		return employee;
-	}
-	
-	@DeleteMapping("/employees/{employeeId}")
-	public boolean deleteEmployee(@PathVariable("employeeId") Long employeeId) {
-		return employeeService.deleteById(employeeId);
-	}
-	
-}
+//package com.example.springboot.rest;
+//
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.DeleteMapping;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.PutMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import com.example.springboot.entity.Employee;
+//import com.example.springboot.service.EmployeeService;
+//
+//@RestController
+//@RequestMapping("/api")
+//public class EmployeeRestController {
+//
+//	private final EmployeeService employeeService;
+//	
+//	@Autowired
+//	public EmployeeRestController(EmployeeService employeeDAO) {
+//		super();
+//		this.employeeService = employeeDAO;
+//	}
+//	
+//	@GetMapping("/employees")
+//	public List<Employee> getEmployees() {
+//		return employeeService.findAll();
+//	}
+//	
+//	@GetMapping("/employees/{employeeId}")
+//	public Employee getEmployee(@PathVariable("employeeId") Long employeeId) {
+//		return employeeService.findById(employeeId);
+//	}
+//	
+//	@PostMapping("/employees")
+//	public Employee saveEmployee(@RequestBody Employee employee) {
+//		employeeService.saveOrUpdate(employee);
+//		return employee;
+//	}
+//	
+//	@PutMapping("/employees")
+//	public Employee updateEmployee(@RequestBody Employee employee) {
+//		employeeService.saveOrUpdate(employee);
+//		return employee;
+//	}
+//	
+//	@DeleteMapping("/employees/{employeeId}")
+//	public boolean deleteEmployee(@PathVariable("employeeId") Long employeeId) {
+//		return employeeService.deleteById(employeeId);
+//	}
+//	
+//}
