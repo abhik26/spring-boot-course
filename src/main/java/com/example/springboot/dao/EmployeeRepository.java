a21d5cc841ff2aa252abe59cb16248d6d9db0d2e
Abhishek Anand <46523310+abhik26@users.noreply.github.com>
2022-11-27 20:07:58 +0530

    Add additional spring properties and code explanation comments.

--- src/main/java/com/example/springboot/dao/EmployeeRepository.java
+++ src/main/java/com/example/springboot/dao/EmployeeRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.example.springboot.entity.Employee;
 
-@RepositoryRestResource(path="employees")
+@RepositoryRestResource(path="employees") // This annotation is used to customize the spring data rest api path for the given repository.
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
 	public List<Employee> findAllByOrderByFirstNameAsc();
